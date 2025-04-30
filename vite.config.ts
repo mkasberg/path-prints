2bbf1ebf5829bec35d293d6d28520e6114f6891c
Wes Bos <wesbos@gmail.com>
2025-04-30 11:56:43 -0400

    deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -6,37 +6,14 @@ import emscriptenStaticWorkerOptions from './vite-fixup-plugin.js'
 
 const __dirname = fileURLToPath(new URL('.', import.meta.url))
 
-console.log(emscriptenStaticWorkerOptions);
-
 export default defineConfig({
   worker: {
     format: 'es',
-    // plugins: () => [emscriptenStaticWorkerOptions() as Plugin]
-  },
-  server: {
-    headers: {
-      'Cross-Origin-Embedder-Policy': 'require-corp',
-      'Cross-Origin-Opener-Policy': 'same-origin',
-    },
   },
   optimizeDeps: {
     exclude: ['manifold-3d']
   },
   build: {
     target: 'esnext',
-    sourcemap: true,
-    rollupOptions: {
-      input: {
-        manifoldCAD: resolve(__dirname, 'index.html'),
-        makeManifold: resolve(__dirname, 'make-manifold.html'),
-        modelViewer: resolve(__dirname, 'model-viewer.html'),
-        three: resolve(__dirname, 'three.html'),
-      },
-      output: {
-        entryFileNames: `assets/[name].js`,
-        chunkFileNames: `assets/[name].js`,
-        assetFileNames: `assets/[name].[ext]`
-      }
-    },
-  },
+  }
 })
