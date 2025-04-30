0e795209732ab894fa2e4f6d7114351c9d145d27
Wes Bos <wesbos@gmail.com>
2025-04-30 17:03:15 -0400

    remove stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,4 @@
-import { resolve } from 'path'
 import { defineConfig } from 'vite'
-import { fileURLToPath } from 'url'
-import type { Plugin } from 'vite'
-import emscriptenStaticWorkerOptions from './vite-fixup-plugin.js'
-
-const __dirname = fileURLToPath(new URL('.', import.meta.url))
 
 export default defineConfig({
   worker: {
